bd534de09d6d09033bb6344bb58debf4452d5472
Dominik Markowski <markowski.dominik.j@gmail.com>
2015-07-27 13:43:54 +0200

    wszystko dziala

--- main_cln.c
+++ main_cln.c
@@ -7,16 +7,36 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <string.h>
 #include <uv.h>
 
-#define DEFAULT_ADDRESS "127.0.0.1"
+#define DEFAULT_ADDRESS "127.0.0.1" //server address
 #define DEFAULT_PORT 1500
 #define BUFFER_SIZE 43776
+#define LOG_ERROR_FILE "catchup_error.log"
+#define LOG_INFO_FILE "catchup_info.log"
 
 #define CHECK(r, msg) if (r) {                                                          \
+    FILE *fd;                                                                           \
+    fd = fopen(LOG_ERROR_FILE, "a");                                                    \
+    if (fd)                                                                             \
+        fprintf(fd, "%s: [%s(%d): %s]\n", msg, uv_err_name((r)), r, uv_strerror((r)));  \
+    fclose(fd);                                                                         \
     fprintf(stderr, "%s: [%s(%d): %s]\n", msg, uv_err_name((r)), r, uv_strerror((r)));  \
-    exit(1);                                                                            \
+    exit(EXIT_FAILURE);                                                                 \
+}
+
+#define LOG_INFO(msg) {                                                                 \
+    time_t rawtime;                                                                     \
+    time (&rawtime);                                                                    \
+    char buffer[20];                                                                    \
+    strftime(buffer,20,"%F %T",localtime(&rawtime));                                    \
+    FILE *fd;                                                                           \
+    fd = fopen(LOG_INFO_FILE, "a");                                                     \
+    if (fd)                                                                             \
+        fprintf(fd, "%s : %s\n", buffer, msg);                                          \
+    fclose(fd);                                                                         \
 }
 
 void on_connect(uv_connect_t*, int);
@@ -68,7 +88,7 @@ void on_connect(uv_connect_t* connect_req, int status) {
     *buf = uv_buf_init((char*) connect_req->handle->data, strlen((char*) connect_req->handle->data));
 
     //send file name
-    uv_write_t write_req; // = malloc(sizeof(uv_write_t));
+    uv_write_t write_req;
     r = uv_write(&write_req, connect_req->handle, buf, 1, NULL);
     CHECK(r, "uv_write");
 
@@ -79,7 +99,6 @@ void on_connect(uv_connect_t* connect_req, int status) {
         CHECK(r, "unlink\n");
     }
 
-    printf("read_start\n");
     r = uv_read_start((uv_stream_t*) connect_req->handle, alloc_buffer, recv_file);
     CHECK(r, "uv_read_start");
 
@@ -87,51 +106,27 @@ void on_connect(uv_connect_t* connect_req, int status) {
 }
 
 void recv_file(uv_stream_t* server, ssize_t nread, const uv_buf_t* buf) {
-    printf("# recv_file, nread: %ld\n", nread);
     uv_fs_t open_req;
     if (nread == UV_EOF) {
-        fprintf(stdout, "  UV_EOF\n");
-        
-        uv_fs_t close_req;
-        r = uv_fs_close(server->loop, &close_req, open_req.result, NULL);
-        CHECK(r, "uv_fs_close");
-
-        //r = uv_loop_close(server->loop);
-        //CHECK(r, "Loop close");
-        
         uv_close((uv_handle_t*) server, NULL);
     } else if (nread > 0) {
-        fprintf(stdout, "  nread %ld, buffer %ld\n", nread, buf->len);
         //fprintf(stdout, "nread %ld, buffer %ld\n%s\n", nread, buf->len, buf->base);
 
         char* data = malloc(sizeof (char)*nread);
         strncpy(data, buf->base, nread);
         uv_buf_t data_buf = uv_buf_init(data, nread);
 
-        fprintf(stdout, "  data_buf %ld\n", data_buf.len);
-
-        printf("  uv_fs_open\n");
         r = uv_fs_open(server->loop, &open_req, server->data, O_CREAT | O_WRONLY | O_APPEND, 0644, NULL);
-        
 
-        printf("  uv_fs_write\n");
         uv_fs_t write_req;
-        //fprintf(stdout, "### buf: %s", write_req.bufs->base);
-        //r = uv_fs_write(server->loop, &write_req, open_req.result, &data_buf, 1, 0, close_file);
-        r = uv_fs_write(server->loop, &write_req, open_req.result, &data_buf, 1, 0, NULL);
-        printf("  # write : %d\n", r);
-        //CHECK(r, "uv_fs_write");
+        uv_fs_write(server->loop, &write_req, open_req.result, &data_buf, 1, 0, NULL);
 
-        printf("  uv_fs_close\n");
         uv_fs_t close_req;
         r = uv_fs_close(server->loop, &close_req, open_req.result, NULL);
         CHECK(r, "uv_fs_close");
 
-        //r = uv_read_stop(server);
-
     }
     if (nread == 0) {
-        fprintf(stdout, "  nread == 0\n");
-        //uv_close((uv_handle_t*) server, NULL);
+        uv_close((uv_handle_t*) server, NULL);
     }
 }
